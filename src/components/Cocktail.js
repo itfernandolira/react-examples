3d010249c09dab48eed5c7e6e5fa5ee4fe9ced64
Fernando Lira <it.fernandolira@gmail.com>
2023-12-20 19:17:36 +0000

    Cocktail App - with React Query

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,50 +1,36 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 
-const Cocktail = props => {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [cocktails, setCocktails] = useState([]);
-
-    useEffect(() => {
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f="+props.letter)
-            .then(res => res.json())
-            .then(
-                (data) => {
-                    setIsLoaded(true);
-                    setCocktails(data.drinks);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [props.letter]);
-
-    if (error) {
-        return <tr><td>Error: {error.message}</td></tr>;
+const Cocktail = (props) => {
+  const { data: cocktails, error, isLoading } = useQuery(
+    ["cocktails", props.letter],
+    () =>
+      fetch(
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=" +
+          props.letter
+      ).then((res) => res.json()),
+    {
+      // You can set additional options here, such as caching, retries, etc.
     }
-    else
-        if (!isLoaded) {
-            return <tr><td>Loading...</td></tr>;
-        }
-        else {
-            return (
-                <>
-                    {cocktails.map((cocktail, index) => {
-                        return (
-                            <tr key={index}>
-                                <th scope="row">{cocktail.strDrink}</th>
-                                <td>{cocktail.strCategory}</td>
-                                <td>{cocktail.strInstructions}</td>
-                                <td><img src={cocktail.strDrinkThumb} style={{ maxWidth: '100px' }} /></td>
-                            </tr>
-                        );
-                    }
-                    )}
-                </>
-            );
-        }
+  );
 
-}
+  if (error) {
+    return <tr><td>Error: {error.message}</td></tr>;
+  } else if (isLoading) {
+    return <tr><td>Loading...</td></tr>;
+  } else {
+    return (
+      <>
+        {cocktails.drinks.map((cocktail, index) => (
+          <tr key={index}>
+            <th scope="row">{cocktail.strDrink}</th>
+            <td>{cocktail.strCategory}</td>
+            <td>{cocktail.strInstructions}</td>
+            <td><img src={cocktail.strDrinkThumb} style={{ maxWidth: '100px' }} alt={cocktail.strDrink} /></td>
+          </tr>
+        ))}
+      </>
+    );
+  }
+};
 
 export default Cocktail;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
 );
 
