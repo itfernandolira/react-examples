e5bbb3f21b212f2951cf6cbae89d3b2976ad2581
Fernando Lira <it.fernandolira@gmail.com>
2023-12-20 19:35:00 +0000

    Custom Hook - limit to 5 chars

--- src/hooks/useCharLimit.js
+++ src/hooks/useCharLimit.js
@@ -2,7 +2,12 @@ import {useState} from 'react'
 
 const useCharLimit = (value) => {
     const [charValue, setCharValue] = useState(value)    
-    return [charValue, setCharValue];
+    const validateCharacterLimit = (value) => {
+        if (value.length <= 5) {
+            setCharValue(value)
+        }
+    }
+    return [charValue, validateCharacterLimit];
 }
 
 export default useCharLimit;
